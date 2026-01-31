7ad6f0ac15ec8729e7e483dc8fdfeebc82b47c9f
omarabuzor71-dev <omarabuzor71@gmail.com>
2026-02-01 02:35:30 +0300

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,13 @@
+function loginAdmin(){
+  if(document.getElementById('pass').value!=='99799') return alert('خطأ');
+  let o=JSON.parse(localStorage.getItem('orders')||'[]');
+  let d=document.getElementById('orders');d.innerHTML='';
+  o.forEach((x,i)=>{if(x.status=='pending') d.innerHTML+=x.user+' '+x.amount+' <button onclick="approve('+i+')">موافقة</button><br>';});
+}
+function approve(i){
+  let o=JSON.parse(localStorage.getItem('orders'));
+  o[i].status='done';
+  localStorage.setItem('orders',JSON.stringify(o));
+  alert('تمت الموافقة');
+  loginAdmin();
+}
